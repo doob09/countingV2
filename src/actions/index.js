d975cb74fcb02097f248b989d5c22241beb0522e
pv <54299527+doob09@users.noreply.github.com>
2020-05-07 14:59:52 -0500

    adding actions

---
+++ src/actions/index.js
@@ -0,0 +1,15 @@
+// action creators
+
+export const increment = ()=>{
+// return object action
+    return{
+        type: 'INCREMENT'
+    };
+}
+
+export const decrement = ()=>{
+
+    return {
+        type : 'DECREMENT'
+    };
+}
