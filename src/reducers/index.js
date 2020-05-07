269688e5df7905d9bd6cd0ff2bad495642078a9f
pv <54299527+doob09@users.noreply.github.com>
2020-05-07 15:42:52 -0500

    add comebineReducers

---
+++ src/reducers/index.js
@@ -0,0 +1,16 @@
+import {combineReducers} from 'redux';
+
+const countReducer = (value =0 , action)=>{
+    if(action.type === 'INCREMENT'){
+        return value + 1;
+    }
+    if(action.type === 'DECREMENT'){
+        return value -1 ;
+    }
+    return value;
+}
+
+
+export default combineRedecuders({
+    count: countReducer
+});
