0d16f151c2920117cd52c7574ff0be43b7f79478
pv <pv@pop-os.localdomain>
2020-05-07 14:40:03 -0500

    add App.css and button

---
+++ src/Components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+const App =()=>{
+    
+    return(
+        <div className= "container main">
+            <h1>Counting App</h1>
+            <h3>Number</h3>
+            <button className="btn">Increment</button>
+            <button className ="btn">Decrement</button>
+        </div>);
+};
+
+export default App;
